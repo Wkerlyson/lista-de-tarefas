7263f167f7e55e4fc7c88a22f0cdaac3a5bc26d8
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2018-12-25 13:33:00 -0200

    Criação das views; Adapter; RecyclerView.

---
+++ app/src/main/java/com/wkprojetos/listadetarefas/activity/MainActivity.java
@@ -0,0 +1,124 @@
+package com.wkprojetos.listadetarefas.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+
+import com.wkprojetos.listadetarefas.R;
+import com.wkprojetos.listadetarefas.adapter.TarefaAdapter;
+import com.wkprojetos.listadetarefas.helper.RecyclerItemClickListener;
+import com.wkprojetos.listadetarefas.model.Tarefa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private TarefaAdapter tarefaAdapter;
+    private List<Tarefa> listaTarefa = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.recyclerListaDeTarefas);
+
+        configurarEventoDeClick();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AdicionarTarefaActivity.class));
+            }
+        });
+    }
+
+    public void carregarListaDeTarefas(){
+
+        Tarefa tarefa = new Tarefa();
+        tarefa.setNomeTarefa("Ir ao mercado");
+        listaTarefa.add(tarefa);
+
+
+        Tarefa tarefa2 = new Tarefa();
+        tarefa2.setNomeTarefa("Ir ao cinema");
+        listaTarefa.add(tarefa2);
+
+        tarefaAdapter = new TarefaAdapter(listaTarefa);
+
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayout.VERTICAL));
+        recyclerView.setAdapter(tarefaAdapter);
+    }
+
+    private void configurarEventoDeClick(){
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(), recyclerView,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Log.i("clique", "onItemClickListener");
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Log.i("clique", "onLongItemClick");
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        }
+                )
+        );
+    }
+
+    @Override
+    protected void onStart() {
+        carregarListaDeTarefas();
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
